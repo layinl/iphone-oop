48e50604b61888ee3ee28b3151173dddc500c4cc
layinl <layon.costa@protonmail.com>
2024-01-15 10:44:10 -0300

    docs: document App

--- src/me/dio/iphoneoop/app/App.java
+++ src/me/dio/iphoneoop/app/App.java
@@ -1,7 +1,15 @@
 package me.dio.iphoneoop.app;
 
+/**
+ * The App interface is the mother of all apps used in
+ * the iPhone
+ */
 public interface App {
 
+  /**
+   * Runs the specified app
+   * @param app the app to execute
+   */
   public void run(String app);
 
 }
